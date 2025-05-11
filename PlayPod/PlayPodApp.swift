8cf31fe0e172f26f88e2b82046d737db8d4e84cb
arianakenbaeva <ariana.kenbayeva@nu.edu.kz>
2025-05-11 17:11:51 +0400

    Initial Commit

---
+++ PlayPod/PlayPodApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlayPodApp.swift
+//  PlayPod
+//
+//  Created by Ариана Кенбаева on 11.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct PlayPodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PlayPodTests/PlayPodTests.swift
@@ -0,0 +1,17 @@
+//
+//  PlayPodTests.swift
+//  PlayPodTests
+//
+//  Created by Ариана Кенбаева on 11.05.2025.
+//
+
+import Testing
+@testable import PlayPod
+
+struct PlayPodTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
